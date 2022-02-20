9ea56d49199b8744f8be56a4729d90d5fafe1ca2
austinkinnear <97315371+austinkinnear@users.noreply.github.com>
2022-02-19 22:09:56 -1000

    Create Request.php

---
+++ Request.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><meta charset="utf-8">
+	<title>SaveTheElephants</title>
+</head>
+<body data-gr-ext-installed="" data-new-gr-c-s-check-loaded="14.994.0">
+
+<header>
+<h1>Contact Recipt</h1>
+</header>
+
+
+<nav>
+    <link rel="stylesheet" href="/finalproject/final-css/finalform.css">
+    <link rel="icon" href="/finalproject/final-photos/elephant-side.png" type="image/png">
+</nav>
+
+
+<main>
+  <p>Thank you  
+  
+  <?php
+  echo $_GET["firstname"]; ?>   <?php echo $_POST["firstname"]; 
+  ?> 
+  your comment is valued by our teams. 
+  </p>
+  
+<p>Save The Elephants!</p>
+  
+
+</main>
+
+
+<footer>
+    <div class="foot">
+   <button type="button" onclick="window.location.href = 'https://c0088.is3.aws.cis.byuh.edu/finalproject/page1.html';">Return</button>
+   </div>
+</footer>
+</body>
+</html>
